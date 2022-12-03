f0247947191f1c73c6adfdac0de38fdb5d7c3831
TonyShi12345 <asd19970623@163.com>
2022-12-02 20:33:00 -0500

    add updateinfo page

--- routes/applicants.js
+++ routes/applicants.js
@@ -146,4 +146,73 @@ router
       });
     }
   })
+  router.route("/updateInfo").get(async (req, res) => {
+    if (req.session.userType === true) {
+      if (req.session.basicInfo === true) {
+        let array=[];
+        let user = await userData.getUserById(req.session.userId);
+        array.push(user);
+        console.log(user)
+        res.render("updateInfo",{user:array}) 
+      } else {
+        res.render("applicantBasicInfo", {
+          title: "Applicant Basic Info",
+        });
+      }
+    } else {
+      return res.status(403).render("forbiddenAccess", {
+        title: "Forbidden Access",
+        error: "Error: 403, You are NOT logged in yet!",
+      });
+    }
+  })
+  .post(async (req, res) => {
+    if (req.session.userType === true) {
+      if (req.session.basicInfo === true) {
+        try {
+        let firstname = req.body.firstnameInput;
+        let lastname = req.body.lastnameInput;
+        let gender = req.body.genderInput;
+        let city = req.body.cityInput;
+        let state = req.body.stateInput;
+        let country = req.body.countryInput;
+        let phone = req.body.phoneInput;
+  
+        const addInfo = await userData.addBasicInfo(
+          req.session.userId,
+          req.session.userType,
+          firstname,
+          lastname,
+          gender,
+          city,
+          state,
+          country,
+          phone
+        );
+        
+        if (addInfo) {
+          req.session.basicInfo = addInfo.basicInfo;
+          res.redirect("/applicant/profile");
+        } else {
+          return res.status(500).json({ error: "Internal Server Error" });
+        }
+      }catch(e){
+        let array=[];
+        let user = await userData.getUserById(req.session.userId);
+        array.push(user);
+        console.log(user)
+        res.render("updateInfo",{error:e,user:array});
+      }
+      } else {
+        res.render("applicantBasicInfo", {
+          title: "Applicant Basic Info",
+        });
+      }
+    } else {
+      return res.status(403).render("forbiddenAccess", {
+        title: "Forbidden Access",
+        error: "Error: 403, You are NOT logged in yet!",
+      });
+    }
+  })
 module.exports = router;
