50f4bb5e75b3a616db76d16c1aa79a4d31163f60
oberonbot <oberonbot@gmail.com>
2022-12-03 15:12:54 -0500

    Add some content back and bug fixed
    Due to merge branch conflict, the content about creating resume, reviewing resume and reviewing applied were disgarded, besides there was a mistake that I made that, I appended the createResume.post to the wrong route, so I repaired it and uploaded other routes.

--- routes/applicants.js
+++ routes/applicants.js
@@ -12,6 +12,7 @@ router.route("/jobmarket").get(async (req, res) => {
         title: "Homepage",
         time: new Date().toUTCString(),
         isHomepage: true,
+        isApplicant: true,
       });
     } else {
       res.render("applicantBasicInfo", {
@@ -219,4 +220,117 @@ router
       });
     }
   })
+
+router.route("/createResume").get(async (req, res) => {
+  if (req.session.userType === true) {
+    if (req.session.basicInfo === true) {
+      res.render("createResume", {
+        title: "Create Resume",
+        isHomepage: true,
+        isApplicant: true,
+      });
+    } else {
+      res.render("applicantBasicInfo", {
+        title: "Applicant Basic Info",
+      });
+    }
+  } else {
+    return res.status(403).render("forbiddenAccess", {
+      title: "Forbidden Access",
+      error: "Error: 403, You are NOT logged in yet!",
+    });
+  }
+})
+.post(async (req, res) => {
+  if (req.session.userType === true) {
+    if (req.session.basicInfo === true) {
+      try {
+        let resumeName = req.body.resumeNameInput;
+        let firstname = req.body.firstnameInput;
+        let lastname = req.body.lastnameInput;
+        let email = req.body.emailInput;
+        let gender = req.body.genderInput;
+        let city = req.body.cityInput;
+        let state = req.body.stateInput;
+        let country = req.body.countryInput;
+        let age = req.body.ageInput;
+        let phone = req.body.phoneInput;
+        let address = req.body.addrInput;
+        let website = req.body.websiteInput;
+        let skills = req.body.skills;
+        // TODO: Haven't finished this part yet.
+
+        const createInfo = await resumeData.createResume(
+          req.session.userId,
+          req.session.userType,
+          firstname,
+          lastname,
+          gender,
+          city,
+          state,
+          country,
+          phone
+        );
+
+        if (createInfo) {
+          res.redirect("/applicant/reviewResumes");
+        } else {
+          return res.status(500).json({ error: "Internal Server Error" });
+        }
+      } catch (e) {
+        res.render("createResume", { error: e });
+      }
+    } else {
+      res.render("applicantBasicInfo", {
+        title: "Applicant Basic Info",
+      });
+    }
+  } else {
+    return res.status(403).render("forbiddenAccess", {
+      title: "Forbidden Access",
+      error: "Error: 403, You are NOT logged in yet!",
+    });
+  }
+});
+router.route("/applied").get(async (req, res) => {
+  if (req.session.userType === true) {
+    if (req.session.basicInfo === true) {
+      res.render("applied", {
+        title: "Applied Jobs",
+        isHomepage: true,
+        isApplicant: true,
+      });
+    } else {
+      res.render("applicantBasicInfo", {
+        title: "Applicant Basic Info",
+      });
+    }
+  } else {
+    return res.status(403).render("forbiddenAccess", {
+      title: "Forbidden Access",
+      error: "Error: 403, You are NOT logged in yet!",
+    });
+  }
+});
+
+router.route("/reviewResumes").get(async (req, res) => {
+  if (req.session.userType === true) {
+    if (req.session.basicInfo === true) {
+      res.render("reviewResumes", {
+        title: "Review Resumes",
+        isHomepage: true,
+        isApplicant: true,
+      });
+    } else {
+      res.render("applicantBasicInfo", {
+        title: "Applicant Basic Info",
+      });
+    }
+  } else {
+    return res.status(403).render("forbiddenAccess", {
+      title: "Forbidden Access",
+      error: "Error: 403, You are NOT logged in yet!",
+    });
+  }
+});
 module.exports = router;
