d7a19ded28bad47f103637a74b14fe4f1c9bc607
Peko <pekko1658@gmail.com>
2022-12-16 15:59:54 -0500

    hr received applications

--- routes/applications.js
+++ routes/applications.js
@@ -12,7 +12,7 @@ const userData = data.users;
 router
     .route("/:id")
     .get(async (req, res) => {
-        if (req.session.userId) {
+        if (!req.session.userId) {
             return res.status(403).render("forbiddenAccess", {
                 title: "Forbidden Access",
                 error: "Error: 403, You are NOT logged in yet!",
@@ -49,7 +49,7 @@ router
 router
     .route("/createApplication")
         .post( async (req, res) => {
-            if (req.session.userType != true) {
+            if (!req.session.userId) {
                 return res.status(403).render("forbiddenAccess", {
                     title: "Forbidden Access",
                     error: "Error: 403, You are NOT logged in yet!",
@@ -78,7 +78,7 @@ router
 router
     .route("/sendNotes") // id is application id
     .post(async (req, res) => {
-        if (req.session.userType != true) {
+        if (!req.session.userId) {
             return res.status(403).render("forbiddenAccess", {
                 title: "Forbidden Access",
                 error: "Error: 403, You are NOT logged in yet!",
@@ -128,7 +128,7 @@ router
 
 router.route("/updateStatus")
     .post(async (req, res) => {
-        if (req.session.userType != true) {
+        if (!req.session.userId) {
             return res.status(403).render("forbiddenAccess", {
                 title: "Forbidden Access",
                 error: "Error: 403, You are NOT logged in yet!",
