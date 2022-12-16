9011cd9a1dd3c32eabc7ce423405e131beba63ae
Peko <pekko1658@gmail.com>
2022-12-16 15:58:17 -0500

    hr received applications

--- routes/applications.js
+++ routes/applications.js
@@ -12,8 +12,8 @@ const userData = data.users;
 router
     .route("/:id")
     .get(async (req, res) => {
-        if (req.session.userType === true || req.session.userType === null) {
-            res.status(403).render("forbiddenAccess", {
+        if (req.session.userType != true) {
+            return res.status(403).render("forbiddenAccess", {
                 title: "Forbidden Access",
                 error: "Error: 403, You are NOT logged in yet!",
             });
